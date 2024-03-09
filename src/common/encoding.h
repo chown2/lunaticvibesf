ea9a679f7f0e3cf20dbaf4a1c50fac63aa8a0e52
chown <mystarwarslegoset@gmail.com>
2024-03-09 00:50:22 +0000

    Revert "refactor(common): string_view-ify encoding.cpp"
    This reverts commit 26516636e23583d68e8a3fe42cab7742c5038301.
    
    this doesn't build lul

--- src/common/encoding.h
+++ src/common/encoding.h
@@ -1,9 +1,4 @@
 #pragma once
-
-#include <istream>
-#include <string>
-#include <string_view>
-
 #include "types.h"
 
 enum class eFileEncoding
@@ -17,11 +12,11 @@ eFileEncoding getFileEncoding(const Path& path);
 eFileEncoding getFileEncoding(std::istream& is);
 const char* getFileEncodingName(eFileEncoding enc);
 
-std::string to_utf8(std::string_view str, eFileEncoding fromEncoding);
-std::string from_utf8(std::string_view input, eFileEncoding toEncoding);
-std::u32string to_utf32(std::string_view str, eFileEncoding fromEncoding);
+std::string to_utf8(const std::string& str, eFileEncoding fromEncoding);
+std::string from_utf8(const std::string& input, eFileEncoding toEncoding);
+std::u32string to_utf32(const std::string& str, eFileEncoding fromEncoding);
 std::string from_utf32(const std::u32string& input, eFileEncoding toEncoding);
 
-std::u32string utf8_to_utf32(const std::string_view input);
+std::u32string utf8_to_utf32(const std::string& input);
 std::string utf32_to_utf8(const std::u32string& str);
 
