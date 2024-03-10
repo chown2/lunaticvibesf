95263d71c7d34f7d411ec677e66e1a3f46921065
chown <mystarwarslegoset@gmail.com>
2024-03-10 00:32:49 +0000

    test(common): add more cases ResolvesLr2PathsCorrectlty

--- test/common/test_path.cpp
+++ test/common/test_path.cpp
@@ -4,23 +4,38 @@
 
 TEST(Paths, ResolvesLr2PathsCorrectlty)
 {
-#ifdef _WIN32
-    GTEST_SKIP() << "skipping the test on Windows as it expects Unix-style paths";
-#endif // _WIN32
-
     // TODO: ban absolute paths.
     EXPECT_EQ(convertLR2Path("/home/me/lv", "/etc/passwd"), "/etc/passwd");
     EXPECT_EQ(convertLR2Path("/opt/lv", "/opt/lv/LR2files/Config/config.xml"), "/opt/lv/LR2files/Config/config.xml");
+#ifdef _WIN32
+    EXPECT_EQ(convertLR2Path("D:\\Games\\LunaticVibes", "C:\\something\\somewhere"), "C:\\something\\somewhere");
+    EXPECT_EQ(convertLR2Path("D:\\Games\\LunaticVibes", R"(D:\Games\LunaticVibes\LR2files\Config\config.xml)"),
+              R"(D:\Games\LunaticVibes\LR2files\Config\config.xml)");
+#else
+    EXPECT_EQ(convertLR2Path("D:\\Games\\LunaticVibes", "C:\\something\\somewhere"), "C:/something/somewhere");
+    EXPECT_EQ(convertLR2Path("D:\\Games\\LunaticVibes", R"(D:\Games\LunaticVibes\LR2files\Config\config.xml)"),
+              R"(D:/Games/LunaticVibes/LR2files/Config/config.xml)");
+#endif // _WIN32
 
     EXPECT_EQ(convertLR2Path("/home/me/lv", ""), "");
     EXPECT_EQ(convertLR2Path("/home/me/lv", "."), ".");
     EXPECT_EQ(convertLR2Path("/home/me/lv", " "), " ");
     EXPECT_EQ(convertLR2Path("/home/me/lv", "  "), "  ");
+    EXPECT_EQ(convertLR2Path("D:\\Games\\LunaticVibes", ""), "");
+    EXPECT_EQ(convertLR2Path("D:\\Games\\LunaticVibes", "."), ".");
+    EXPECT_EQ(convertLR2Path("D:\\Games\\LunaticVibes", " "), " ");
+    EXPECT_EQ(convertLR2Path("D:\\Games\\LunaticVibes", "  "), "  ");
 
     // FIXME: quotes only fail with global-buffer-overflow.
     // EXPECT_EQ(convertLR2Path("/home/me/lv", R"(" "");
     // EXPECT_EQ(convertLR2Path("/home/me/lv", R"("" ");
     // EXPECT_EQ(convertLR2Path("/home/me/lv", R"(""""""""""")"), "");
+#ifdef _WIN32
+    EXPECT_EQ(
+        convertLR2Path("D:\\Games\\LunaticVibes", R"(.\LR2files\Theme\EndlessCirculation\Play\parts\Combo\*.png""")"),
+        R"(D:\Games\LunaticVibes\LR2files\Theme\EndlessCirculation\Play\parts\Combo\*.png)");
+#else
     EXPECT_EQ(convertLR2Path("/home/me/lv", R"(.\LR2files\Theme\EndlessCirculation\Play\parts\Combo\*.png""")"),
               "/home/me/lv/LR2files/Theme/EndlessCirculation/Play/parts/Combo/*.png");
+#endif // _WIN32
 }
