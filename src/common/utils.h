168326cac2e0f844be30f90654bf28c738c372dc
chown <mystarwarslegoset@gmail.com>
2024-03-12 23:16:28 +0000

    refactor(common): nodiscard things

--- src/common/utils.h
+++ src/common/utils.h
@@ -14,62 +14,62 @@ namespace fs = std::filesystem;
 inline Path executablePath;
 
 // path may include wildcard (*, ?)
-std::vector<Path> findFiles(Path path, bool recursive = false);
+[[nodiscard]] std::vector<Path> findFiles(Path path, bool recursive = false);
 
 [[nodiscard]] bool isParentPath(const Path& parent, const Path& dir);
 
 // string to int
-int toInt(std::string_view str, int defVal = 0) noexcept;
+[[nodiscard]] int toInt(std::string_view str, int defVal = 0) noexcept;
 // string to double
-double toDouble(std::string_view str, double defVal = 0.0) noexcept;
+[[nodiscard]] double toDouble(std::string_view str, double defVal = 0.0) noexcept;
 
 namespace lunaticvibes {
 // strcasecmp
-bool iequals(std::string_view lhs, std::string_view rhs) noexcept;
+[[nodiscard]] bool iequals(std::string_view lhs, std::string_view rhs) noexcept;
 } // namespace lunaticvibes
 
-constexpr unsigned base36(char c)
+[[nodiscard]] constexpr unsigned base36(char c)
 {
 	return (c > '9') ? (c >= 'a' ? 10 + c - 'a' : 10 + c - 'A') : (c - '0');
 }
 
-constexpr unsigned base36(char first, char second)
+[[nodiscard]] constexpr unsigned base36(char first, char second)
 {
 	return 36 * base36(first) + base36(second);
 }
 
-constexpr unsigned base36(const char* c)
+[[nodiscard]] constexpr unsigned base36(const char* c)
 {
 	return base36(c[0], c[1]);
 }
 
-constexpr unsigned base16(char c)
+[[nodiscard]] constexpr unsigned base16(char c)
 {
 	return (c > '9') ? (c >= 'a' ? 10 + c - 'a' : 10 + c - 'A') : (c - '0');
 }
 
-constexpr unsigned base16(char first, char second)
+[[nodiscard]] constexpr unsigned base16(char first, char second)
 {
 	return 16 * base16(first) + base16(second);
 }
 
-constexpr unsigned base16(const char* c)
+[[nodiscard]] constexpr unsigned base16(const char* c)
 {
 	return base16(c[0], c[1]);
 }
 
-std::string bin2hex(const void* bin, size_t size);
-std::string hex2bin(std::string_view hex);
+[[nodiscard]] std::string bin2hex(const void* bin, size_t size);
+[[nodiscard]] std::string hex2bin(std::string_view hex);
 
-std::string toLower(std::string_view s);
-std::string toUpper(std::string_view s);
+[[nodiscard]] std::string toLower(std::string_view s);
+[[nodiscard]] std::string toUpper(std::string_view s);
 
-std::string convertLR2Path(const std::string& lr2path, const Path& relative_path);
-std::string convertLR2Path(const std::string& lr2path, const std::string& relative_path_utf8);
-std::string convertLR2Path(const std::string& lr2path, const char* relative_path_utf8);
-std::string convertLR2Path(const std::string& lr2path, std::string_view relative_path_utf8);
+[[nodiscard]] std::string convertLR2Path(const std::string& lr2path, const Path& relative_path);
+[[nodiscard]] std::string convertLR2Path(const std::string& lr2path, const std::string& relative_path_utf8);
+[[nodiscard]] std::string convertLR2Path(const std::string& lr2path, const char* relative_path_utf8);
+[[nodiscard]] std::string convertLR2Path(const std::string& lr2path, std::string_view relative_path_utf8);
 
-Path PathFromUTF8(std::string_view s);
+[[nodiscard]] Path PathFromUTF8(std::string_view s);
 
 void preciseSleep(long long nanoseconds);
 
