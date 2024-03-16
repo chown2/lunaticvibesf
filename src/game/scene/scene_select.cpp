chown2/lunaticvibesf

commit 448848c0836fd7279973101ee473b6ba941b0671

fix(game): cancel direct preview loading on scene change
TODO: this needs to be ported to the play scene.

src/game/scene/scene_select.cpp
@@ -3110,9 +3110,8 @@ void SceneSelect::updatePreview()
                         std::count_if(bms->wavFiles.begin(), bms->wavFiles.end(), std::not_fn(&std::string::empty));
                     if (wavTotal != 0)
                     {
-                        // FIXME: also account for scene changes. As is, it hangs while loading samples on scene change.
                         static const auto shouldDiscard = [](SceneSelect& s, const std::shared_ptr<ChartFormatBMS>& bms) {
-                            if (gAppIsExiting) return true;
+                            if (gAppIsExiting || gNextScene != SceneType::PLAY) return true;
                             // Preview chart changed, don't process remaining files.
                             if (std::shared_lock l(s.previewMutex); bms != s.previewChart) return true;
                             return false;

commit 30bcc157d93ba133b22a414921abda07c340581f

fix(game): check for correct scene when checking preview discarding
Fixes preview not playing basically ever.

Fixes: 448848c0836f ("fix(game): cancel direct preview loading on scene change")

src/game/scene/scene_select.cpp
@@ -3111,8 +3111,7 @@ void SceneSelect::updatePreview()
                     if (wavTotal != 0)
                     {
                         static const auto shouldDiscard = [](SceneSelect& s, const std::shared_ptr<ChartFormatBMS>& bms) {
-                            if (gAppIsExiting || gNextScene != SceneType::PLAY) return true;
-                            // Preview chart changed, don't process remaining files.
+                            if (gAppIsExiting || gNextScene != SceneType::SELECT) return true;
                             if (std::shared_lock l(s.previewMutex); bms != s.previewChart) return true;
                             return false;
                         };
@@ -3139,7 +3138,7 @@ void SceneSelect::updatePreview()
 
                         if (shouldDiscard(*this, bms))
                         {
-                            LOG_DEBUG << "[Select] Preview chart has changed, discarding";
+                            LOG_DEBUG << "[Select] Scene or preview chart has changed, discarding";
                             return;
                         }
 