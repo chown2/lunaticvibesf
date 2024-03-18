chown2/lunaticvibesf

commit d0cc1d6e04027352011a175c5c8d51bbdb285b29

fix(common): handle paths starting with ../ in resolveCaseInsensitivePath

src/common/utils.cpp
@@ -357,7 +357,13 @@ static std::string resolveCaseInsensitivePath(std::string input)
             continue;
         }
 
+        std::string_view prefix;
         if (segment == ".") {
+            prefix = CURRENT_PATH_RELATIVE_PREFIX;
+        } else if (segment == "..") {
+            prefix = "../";
+        }
+        if (!prefix.empty()) {
             if (!out.empty() && out.back() != '/') {
                 out += '/';
             }

commit b5d1c2ff876100101c90aa1b77676098d482d3b2

fix(common): replace ../ with itself instead of ./
Fixes default Lunatic Vibes key config which references
`LR2files/../gamedata`.

Fixes: d0cc1d6e0402 ("fix(common): handle paths starting with ../ in resolveCaseInsensitivePath")

src/common/utils.cpp
@@ -367,7 +367,7 @@ static std::string resolveCaseInsensitivePath(std::string input)
             if (!out.empty() && out.back() != '/') {
                 out += '/';
             }
-            out += CURRENT_PATH_RELATIVE_PREFIX;
+            out += prefix;
             segments_traversed += 1;
             continue;
         }