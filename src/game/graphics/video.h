chown2/lunaticvibesf

commit fa2582b42b5c5c42be569b4b4e52cbfee9cb0b08

refactor: use frame_num instead of frame_number
> ‘AVCodecContext::frame_number’ is deprecated

There's a missing header here, he-he.

src/game/graphics/video.h
@@ -61,7 +61,7 @@ private:
 	AVFrame *pFrame = nullptr;
 	AVPacket *pPacket = nullptr;
 	int videoIndex = -1;
-	unsigned decoded_frames = 0;
+	int64_t decoded_frames = 0;
 	std::chrono::time_point<std::chrono::system_clock> startTime;
 	std::future<void> decodeEnd;
 

src/game/graphics/video.cpp
@@ -245,7 +245,7 @@ void sVideo::decodeLoop()
 					}
 				}
 			}
-			decoded_frames = pCodecCtx->frame_number;
+			decoded_frames = pCodecCtx->frame_num;
 		}
 
 		if (loop_playback)
@@ -276,7 +276,7 @@ void sVideo::decodeLoop()
 					av_frame_ref(pFrame, pFrame1);
 					valid = true;
 				}
-				decoded_frames = pCodecCtx->frame_number;
+				decoded_frames = pCodecCtx->frame_num;
 			}
 
 			playing = false;

commit e21ce64e5240607be7171dab3a71f8007c753fcd

fix(game): use correct return type for trivial getter
Fixes: fa2582b42b5c ("refactor: use frame_num instead of frame_number")

src/game/graphics/video.h
@@ -95,8 +95,8 @@ public:
 	virtual ~sVideo();
 	int setVideo(const Path& file, double speed, bool loop = false);
 	int unsetVideo();
-	int getW() { return w; }
-	int getH() { return h; }
+	int getW() const { return w; }
+	int getH() const { return h; }
 	bool isPlaying() const { return playing; }
 
 public:
@@ -109,8 +109,8 @@ public:
 	void stopPlaying();
 	void decodeLoop();
 
-	int getDecodedFrames() { return decoded_frames; }
-	AVFrame* getFrame() { return valid ? pFrame.get() : NULL; }
+	int64_t getDecodedFrames() const { return decoded_frames; }
+	AVFrame* getFrame() const { return valid ? pFrame.get() : NULL; }
 
 public:
 	std::shared_mutex video_frame_mutex;