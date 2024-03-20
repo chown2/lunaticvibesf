chown2/lunaticvibesf

commit 5c12b404d014dad4a39bb318d41d329807535850

refactor: fix GCC's -Wall -Wno-unknown-pragmas
Mismatched variable types and unused variables, sign-compare - different
signedness comparisons, parentheses - explicit parentheses.

src/game/skin/skin_lr2_button_callbacks.cpp
@@ -531,13 +531,12 @@ void pitch_type(int plus)
 // 40, 41
 void gauge_type(int player, int plus)
 {
-    int slot = 0;
     IndexOption op;
     IndexText tx;
     switch (player)
     {
-    case 0: slot = PLAYER_SLOT_PLAYER; op = IndexOption::PLAY_GAUGE_TYPE_1P; tx = IndexText::GAUGE_1P; break;
-    case 1: slot = PLAYER_SLOT_TARGET; op = IndexOption::PLAY_GAUGE_TYPE_2P; tx = IndexText::GAUGE_2P; break;
+    case 0: op = IndexOption::PLAY_GAUGE_TYPE_1P; tx = IndexText::GAUGE_1P; break;
+    case 1: op = IndexOption::PLAY_GAUGE_TYPE_2P; tx = IndexText::GAUGE_2P; break;
     default: return;
     }
 
@@ -1046,8 +1045,10 @@ void favorite_ignore(int plus)
 }
 
 // 91 - 96
-void difficulty(int diff, int plus)
+void difficulty(int diff_, int plus)
 {
+    auto diff = static_cast<unsigned>(diff_);
+
     if (diff == gSelectContext.filterDifficulty)
     {
         std::unique_lock l(gSelectContext._mutex);
@@ -1389,9 +1390,10 @@ static void open_ir_page()
     LOG_DEBUG << "opened the thingy";
 }
 
-void help(int index)
+void help(int index_)
 {
-    assert(index < 10);
+    assert(index_ > 0 && index_ < 10);
+    auto index = static_cast<unsigned>(index_);
     lunaticvibes::Time now{};
 
     std::shared_ptr<SkinLR2> s = std::dynamic_pointer_cast<SkinLR2>(SkinMgr::get(SkinType::MUSIC_SELECT));

src/common/chartformat/chartformat.h
@@ -46,7 +46,7 @@ public:
     HashMD5 fileHash;
     HashMD5 folderHash;
     long long addTime = 0;  // from epoch time
-    int gamemode = 7;       // 5, 7, 9, 10, 14, 24?, 48?
+    unsigned gamemode = 7;  // 5, 7, 9, 10, 14, 24?, 48?
 
     StringContent title;
     StringContent title2;
@@ -55,7 +55,7 @@ public:
     StringContent genre;
     StringContent version;     // mostly known as difficulty name
     int playLevel = 0;
-    int difficulty = 3;            // N/H/A
+    unsigned difficulty = 3; // N/H/A
     double levelEstimated = 0.0;
 
     int totalLength = 0;// in seconds

src/common/chartformat/chartformat_bms.cpp
@@ -122,7 +122,7 @@ int ChartFormatBMS::initWithFile(const Path& filePath, uint64_t randomSeed)
             auto spacePos = std::min(buf.length(), buf.find_first_of(' '));
 
             // supporting single level control flow (#RANDOM, #IF, etc.), matching with LR2's capability
-            if (!randomUsedValues.empty() && randomUsedValues.top().size() < randomValue.top())
+            if (!randomUsedValues.empty() && static_cast<int>(randomUsedValues.top().size()) < randomValue.top())
             {
                 StringContentView v = buf;
                 if (ifValue.empty() && !v.empty() && v[0] == '#' &&
@@ -222,7 +222,7 @@ int ChartFormatBMS::initWithFile(const Path& filePath, uint64_t randomSeed)
                     }
                 }
 
-                if (!randomUsedValues.empty() && randomUsedValues.top().size() == randomValueMax.top())
+                if (!randomUsedValues.empty() && static_cast<int>(randomUsedValues.top().size()) == randomValueMax.top())
                 {
                     randomValueMax.pop();
                     randomValue.pop();
@@ -566,7 +566,7 @@ int ChartFormatBMS::initWithFile(const Path& filePath, uint64_t randomSeed)
             { R"((?i)(hard|hyper))" },
             { R"((?i)(ex|another|insane|lunatic|maniac))" },
         };
-        difficulty = -1;
+        difficulty = 2; // defaults to normal
         for (int i = 4; i >= 1; --i)
         {
             if (RE2::PartialMatch(version, *difficultyRegex[i]))
@@ -585,8 +585,6 @@ int ChartFormatBMS::initWithFile(const Path& filePath, uint64_t randomSeed)
                 break;
             }
         }
-        if (difficulty == -1)
-            difficulty = 2; // defaults to normal
     }
 
     for (size_t i = 0; i <= lastBarIdx; i++)

src/common/difficultytable/table_bms.cpp
@@ -184,8 +184,7 @@ GetResult GET(const std::string& url, std::string& result)
 
 void DifficultyTableBMS::updateFromUrl(std::function<void(DifficultyTable::UpdateResult)> finishedCallback)
 {
-	if (!lunaticvibes::iequals(url.substr(0, 7), "http://") && !lunaticvibes::iequals(url.substr(0, 8), "https://") ||
-		url.substr(8).find('/') == url.npos)
+	if (!lunaticvibes::iequals(url.substr(0, 7), "http://") && !lunaticvibes::iequals(url.substr(0, 8), "https://"))
 	{
 		LOG_ERROR << "[TableBMS] URL error: " << url;
 		finishedCallback(UpdateResult::WEB_PATH_ERROR);

src/db/db_song.cpp
@@ -97,43 +97,40 @@ struct song_all_params
 
     song_all_params(const std::vector<std::any>& queryResult)
     {
-        try 
-        {
-            md5         = ANY_STR(queryResult.at(0));
-            parent      = ANY_STR(queryResult.at(1));
-            file        = ANY_STR(queryResult.at(2));
-            type        = ANY_INT(queryResult.at(3));
-            title       = ANY_STR(queryResult.at(4));
-            title2      = ANY_STR(queryResult.at(5));
-            artist      = ANY_STR(queryResult.at(6));
-            artist2     = ANY_STR(queryResult.at(7));
-            genre       = ANY_STR(queryResult.at(8));
-            version     = ANY_STR(queryResult.at(9));
-            level       = ANY_REAL(queryResult.at(10));
-            bpm         = ANY_REAL(queryResult.at(11));
-            minbpm      = ANY_REAL(queryResult.at(12));
-            maxbpm      = ANY_REAL(queryResult.at(13));
-            length      = ANY_INT(queryResult.at(14));
-            totalnotes  = ANY_INT(queryResult.at(15));
-            stagefile   = ANY_STR(queryResult.at(16));
-            bannerfile  = ANY_STR(queryResult.at(17));
-            gamemode    = ANY_INT(queryResult.at(18));
-            judgerank   = ANY_INT(queryResult.at(19));
-            total       = ANY_INT(queryResult.at(20));
-            playlevel   = ANY_INT(queryResult.at(21));
-            difficulty  = ANY_INT(queryResult.at(22));
-            longnote    = ANY_INT(queryResult.at(23));
-            landmine    = ANY_INT(queryResult.at(24));
-            metricmod   = ANY_INT(queryResult.at(25));
-            stop        = ANY_INT(queryResult.at(26));
-            bga         = ANY_INT(queryResult.at(27));
-            random      = ANY_INT(queryResult.at(28));
-            addtime     = ANY_INT(queryResult.at(29));
-        }
-        catch (std::out_of_range&)
-        {
-        }
-    }   
+        if (queryResult.size() < SONG_PARAM_COUNT)
+            return;
+
+        md5         = ANY_STR(queryResult[0]);
+        parent      = ANY_STR(queryResult[1]);
+        file        = ANY_STR(queryResult[2]);
+        type        = ANY_INT(queryResult[3]);
+        title       = ANY_STR(queryResult[4]);
+        title2      = ANY_STR(queryResult[5]);
+        artist      = ANY_STR(queryResult[6]);
+        artist2     = ANY_STR(queryResult[7]);
+        genre       = ANY_STR(queryResult[8]);
+        version     = ANY_STR(queryResult[9]);
+        level       = ANY_REAL(queryResult[10]);
+        bpm         = ANY_REAL(queryResult[11]);
+        minbpm      = ANY_REAL(queryResult[12]);
+        maxbpm      = ANY_REAL(queryResult[13]);
+        length      = ANY_INT(queryResult[14]);
+        totalnotes  = ANY_INT(queryResult[15]);
+        stagefile   = ANY_STR(queryResult[16]);
+        bannerfile  = ANY_STR(queryResult[17]);
+        gamemode    = ANY_INT(queryResult[18]);
+        judgerank   = ANY_INT(queryResult[19]);
+        total       = ANY_INT(queryResult[20]);
+        playlevel   = ANY_INT(queryResult[21]);
+        difficulty  = ANY_INT(queryResult[22]);
+        longnote    = ANY_INT(queryResult[23]);
+        landmine    = ANY_INT(queryResult[24]);
+        metricmod   = ANY_INT(queryResult[25]);
+        stop        = ANY_INT(queryResult[26]);
+        bga         = ANY_INT(queryResult[27]);
+        random      = ANY_INT(queryResult[28]);
+        addtime     = ANY_INT(queryResult[29]);
+    }
 };
 bool convert_bms(const std::shared_ptr<ChartFormatBMSMeta>& chart, const std::vector<std::any>& in)
 {

src/game/arena/arena_client.cpp
@@ -445,7 +445,7 @@ void ArenaClient::handlePlayerJoined(const std::shared_ptr<ArenaMessage>& msg)
 	{
 		LOG_WARNING << "[Arena] player ID " << pMsg->playerID << " already exists, removing old";
 
-		for (int i = 0; i < gArenaData.getPlayerCount(); ++i)
+		for (size_t i = 0; i < gArenaData.getPlayerCount(); ++i)
 		{
 			if (gArenaData.playerIDs[i] == pMsg->playerID)
 			{
@@ -478,7 +478,7 @@ void ArenaClient::handlePlayerLeft(const std::shared_ptr<ArenaMessage>& msg)
 	}
 	else
 	{
-		for (int i = 0; i < gArenaData.getPlayerCount(); ++i)
+		for (size_t i = 0; i < gArenaData.getPlayerCount(); ++i)
 		{
 			if (gArenaData.playerIDs[i] == pMsg->playerID)
 			{

src/game/arena/arena_host.cpp
@@ -897,7 +897,7 @@ void ArenaHost::update()
 		for (auto& k : clientsRemoving)
 		{
 			Client& c = clients[k];
-			for (int i = 0; i < gArenaData.getPlayerCount(); ++i)
+			for (size_t i = 0; i < gArenaData.getPlayerCount(); ++i)
 			{
 				if (c.id == gArenaData.getPlayerID(i))
 				{
@@ -981,7 +981,7 @@ void ArenaHost::update()
 	if (!gSelectContext.isArenaReady && !clients.empty() && !hostRequestChartHash.empty())
 	{
 		std::shared_lock l(clientsMutex);
-		int ready = 0;
+		size_t ready = 0;
 		for (auto& [k, cc] : clients)
 		{
 			if (cc.requestChartHash == hostRequestChartHash)
@@ -1021,7 +1021,7 @@ void ArenaHost::update()
 	if (!gArenaData.playing && isLoadingFinished())
 	{
 		std::shared_lock l(clientsMutex);
-		int ready = 0;
+		size_t ready = 0;
 		for (auto& [k, cc] : clients)
 		{
 			if (cc.isLoadingFinished)
@@ -1086,7 +1086,7 @@ void ArenaHost::update()
 	if (gArenaData.playing && isPlayingFinished())
 	{
 		std::shared_lock l(clientsMutex);
-		int finished = 0;
+		size_t finished = 0;
 		for (auto& [k, cc] : clients)
 		{
 			if (cc.isPlayingFinished)
@@ -1111,7 +1111,7 @@ void ArenaHost::update()
 	if (isResultFinished())
 	{
 		std::shared_lock l(clientsMutex);
-		int finished = 0;
+		size_t finished = 0;
 		for (auto& [k, cc] : clients)
 		{
 			if (cc.isResultFinished)

src/game/arena/arena_host.h
@@ -40,7 +40,7 @@ private:
 
 	HashMD5 requestChartPending;
 	std::string requestChartPendingClientKey;		// who has requested this chart
-	int requestChartPendingExistCount = 0;	// if count == clients.size(), inform all clients
+	size_t requestChartPendingExistCount = 0;	// if count == clients.size(), inform all clients
 
 	bool _isLoadingFinished = false; // host status
 	bool _isCreatedRuleset = false;	// host status

commit dd27daa82a7b1d0c9b1341e276d1cb146f4eb8b8

fix(game): allow opening first help page
Fixes: 5c12b404d014 ("refactor: fix GCC's -Wall -Wno-unknown-pragmas")

src/game/skin/skin_lr2_button_callbacks.cpp
@@ -1392,7 +1392,7 @@ static void open_ir_page()
 
 void help(int index_)
 {
-    assert(index_ > 0 && index_ < 10);
+    assert(index_ >= 0 && index_ < 10);
     auto index = static_cast<unsigned>(index_);
     lunaticvibes::Time now{};
 