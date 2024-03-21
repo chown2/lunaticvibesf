chown2/lunaticvibesf

commit 4ff61ab2e3dea1a7cfcf873d531697fb10c9e629

feat: store all and every chart score
This will help with implementing importing from LR2 better, also.

src/db/db_score.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <any>
 #include <cassert>
+#include <exception>
 #include <memory>
 #include <unordered_map>
 #include <vector>
@@ -12,18 +13,70 @@
 #include "common/log.h"
 #include "common/types.h"
 #include "common/hash.h"
+#include "db/db_conn.h"
 
+static constexpr auto&& CREATE_SCORE_HISTORY_BMS_TABLE_STR =
+"CREATE TABLE IF NOT EXISTS score_history_bms( "
+"md5 TEXT NOT NULL, "
+"notes INTEGER NOT NULL, "
+"score INTEGER NOT NULL, "
+"fast INTEGER NOT NULL, "
+"slow INTEGER NOT NULL, "
+"maxcombo INTEGER NOT NULL , "
+"addtime INTEGER NOT NULL , "
+"exscore INTEGER NOT NULL, "
+"lamp INTEGER NOT NULL, "
+"pgreat INTEGER NOT NULL, "
+"great INTEGER NOT NULL, "
+"good INTEGER NOT NULL, "
+"bad INTEGER NOT NULL, "
+"bpoor INTEGER NOT NULL, "
+"miss INTEGER NOT NULL, "
+"bp INTEGER NOT NULL, "
+"cb INTEGER NOT NULL, "
+"playedtime INTEGER NOT NULL, "
+"replay TEXT PRIMARY KEY UNIQUE NOT NULL "
+")";
+
+// Combined score_bms and score_history_bms.
+static constexpr auto&& CREATE_SCORE_CACHE_BMS_TABLE_STR =
+"CREATE TABLE IF NOT EXISTS score_cache_bms( "
+"md5 TEXT PRIMARY KEY UNIQUE NOT NULL, "
+"notes INTEGER NOT NULL, "
+"score INTEGER NOT NULL, "
+"fast INTEGER NOT NULL, "
+"slow INTEGER NOT NULL, "
+"maxcombo INTEGER NOT NULL, "
+"addtime INTEGER NOT NULL, "
+"pc INTEGER NOT NULL, "
+"clearcount INTEGER NOT NULL, "
+"exscore INTEGER NOT NULL, "
+"lamp INTEGER NOT NULL, "
+"pgreat INTEGER NOT NULL, "
+"great INTEGER NOT NULL, "
+"good INTEGER NOT NULL, "
+"bad INTEGER NOT NULL, "
+"bpoor INTEGER NOT NULL, "
+"miss INTEGER NOT NULL, "
+"bp INTEGER NOT NULL, "
+"cb INTEGER NOT NULL, "
+"playedtime INTEGER NOT NULL, "
+"replay TEXT NOT NULL "
+")";
+
+// TODO: rename to like 'legacy_pbs_bms'.
+// TODO: remove default values.
 static constexpr auto&& CREATE_SCORE_BMS_TABLE_STR =
 "CREATE TABLE IF NOT EXISTS score_bms( "
 "md5 TEXT PRIMARY KEY UNIQUE NOT NULL, "
 "notes INTEGER NOT NULL, "
 "score INTEGER NOT NULL, "
-"rate REAL NOT NULL, "
+"rate REAL NOT NULL, " // TODO: remove column
 "fast INTEGER NOT NULL, "
 "slow INTEGER NOT NULL, "
 "maxcombo INTEGER NOT NULL DEFAULT 0, "
 "addtime INTEGER NOT NULL DEFAULT 0, "
-"pc INTEGER NOT NULL DEFAULT 0, "
+"pc INTEGER NOT NULL DEFAULT 0, " // TODO: rename column
 "clearcount INTEGER NOT NULL DEFAULT 0, "
 "exscore INTEGER NOT NULL, "
 "lamp INTEGER NOT NULL, "
@@ -37,7 +90,6 @@ static constexpr auto&& CREATE_SCORE_BMS_TABLE_STR =
 "cb INTEGER NOT NULL, "
 "replay TEXT "
 ")";
-// TODO: add a field like playedtimeseconds, when md5 is no longer UNIQUE.
 static constexpr size_t SCORE_BMS_PARAM_COUNT = 21;
 struct score_bms_all_params
 {
@@ -99,7 +151,6 @@ bool convert_score_bms(ScoreBMS& out, const std::vector<std::any>& in)
     if (in.size() < SCORE_BMS_PARAM_COUNT) return false;
 
     score_bms_all_params params(in);
-
     out.notes = params.notes;
     out.score = params.score;
     out.rate = params.rate;
@@ -169,6 +220,16 @@ ScoreDB::ScoreDB(const char* path): SQLite(path, "SCORE")
         LOG_FATAL << "[ScoreDB] Failed to create table 'score_bms': " << errmsg();
         abort();
     }
+    if (exec(CREATE_SCORE_HISTORY_BMS_TABLE_STR) != SQLITE_OK)
+    {
+        LOG_FATAL << "[ScoreDB] Failed to create table 'score_history_bms': " << errmsg();
+        abort();
+    }
+    if (exec(CREATE_SCORE_CACHE_BMS_TABLE_STR) != SQLITE_OK)
+    {
+        LOG_FATAL << "[ScoreDB] Failed to create table 'score_cache_bms': " << errmsg();
+        abort();
+    }
     if (exec(CREATE_SCORE_COURSE_BMS_TABLE_STR) != SQLITE_OK)
     {
         LOG_FATAL << "[ScoreDB] Failed to create table 'score_course_bms': " << errmsg();
@@ -184,9 +245,16 @@ ScoreDB::ScoreDB(const char* path): SQLite(path, "SCORE")
         LOG_FATAL << "[ScoreDB] Failed to initialize table 'stats': " << errmsg();
         abort();
     }
+
+    {
+        const auto resp = query("SELECT count(*) FROM score_cache_bms");
+        assert(resp.size() == 1);
+        if (ANY_INT(resp[0][0]) == 0)
+            rebuildBmsPbCache();
+    }
 }
 
-void ScoreDB::deleteScoreBMS(const char* tableName, const HashMD5& hash)
+void ScoreDB::deleteLegacyScoreBMS(const char* tableName, const HashMD5& hash)
 {
     int ret;
 
@@ -197,7 +265,7 @@ void ScoreDB::deleteScoreBMS(const char* tableName, const HashMD5& hash)
     ret = exec(static_cast<char*>(sqlbuf), {hashStr});
     if (ret != SQLITE_OK)
     {
-        LOG_ERROR << "[ScoreDB] Failed to delete score: " << errmsg();
+        LOG_ERROR << "[ScoreDB] Failed to delete legacy score: " << errmsg();
         return;
     }
 
@@ -209,7 +277,7 @@ std::shared_ptr<ScoreBMS> ScoreDB::getScoreBMS(const char* tableName, const Hash
     return cache[tableName][hash.hexdigest()];
 }
 
-void ScoreDB::updateScoreBMS(const char* tableName, const HashMD5& hash, const ScoreBMS& score)
+void ScoreDB::updateLegacyScoreBMS(const char* tableName, const HashMD5& hash, const ScoreBMS& score)
 {
     int ret;
 
@@ -288,7 +356,16 @@ void ScoreDB::updateScoreBMS(const char* tableName, const HashMD5& hash, const S
 
 void ScoreDB::deleteChartScoreBMS(const HashMD5& hash)
 {
-    deleteScoreBMS("score_bms", hash);
+    deleteLegacyScoreBMS("score_bms", hash);
+
+    const std::string hash_str = hash.hexdigest();
+    int ret = exec("DELETE FROM score_history_bms WHERE md5=?", {hash_str});
+    if (ret != SQLITE_OK)
+    {
+        LOG_ERROR << "[ScoreDB] Failed to delete score: " << errmsg();
+        return;
+    }
+    cache["score_bms"].erase(hash_str);
 }
 
 std::shared_ptr<ScoreBMS> ScoreDB::getChartScoreBMS(const HashMD5& hash) const
@@ -296,15 +373,213 @@ std::shared_ptr<ScoreBMS> ScoreDB::getChartScoreBMS(const HashMD5& hash) const
     return getScoreBMS("score_bms", hash);
 }
 
-void ScoreDB::updateChartScoreBMS(const HashMD5& hash, const ScoreBMS& score)
+void ScoreDB::updateLegacyChartScoreBMS(const HashMD5& hash, const ScoreBMS& score)
 {
-    updateScoreBMS("score_bms", hash, score);
+    updateLegacyScoreBMS("score_bms", hash, score);
+    rebuildBmsPbCache();
+}
+
+void ScoreDB::insertChartScoreBMS(const HashMD5& hash, const ScoreBMS& score)
+{
+    saveChartScoreBmsToHistory(hash, score);
+    updateCachedChartPbBms(hash, score);
     updateStats(score);
+}
+
+static bool convertCachedPbBms(ScoreBMS& out, const std::vector<std::any>& score) noexcept
+try
+{
+    if (score.size() < 19) return false;
+    // md5 = ANY_STR(score[0]);
+    out.notes = ANY_INT(score[1]);
+    out.score = ANY_INT(score[2]);
+    out.fast = ANY_INT(score[3]);
+    out.slow = ANY_INT(score[4]);
+    out.maxcombo = ANY_INT(score[5]);
+    out.addtime = ANY_INT(score[6]);
+    out.exscore = ANY_INT(score[7]);
+    out.lamp = static_cast<ScoreBMS::Lamp>(ANY_INT(score[8]));
+    out.pgreat = ANY_INT(score[9]);
+    out.great = ANY_INT(score[10]);
+    out.good = ANY_INT(score[11]);
+    out.bad = ANY_INT(score[12]);
+    out.kpoor = ANY_INT(score[13]);
+    out.miss = ANY_INT(score[14]);
+    out.bp = ANY_INT(score[15]);
+    out.combobreak = ANY_INT(score[16]);
+    out.play_time = ANY_INT(score[17]);
+    out.replayFileName = ANY_STR(score[18]);
+
+    out.rate = static_cast<double>(out.exscore) / static_cast<double>(out.notes * 2) * 100;
+    return true;
+}
+catch (const std::exception& e)
+{
+    LOG_ERROR << e.what();
+    return false;
+}
+
+[[nodiscard]] std::shared_ptr<ScoreBMS> ScoreDB::fetchCachedPbBMS(const HashMD5& hash) const
+{
+    auto out = std::make_shared<ScoreBMS>();
+    const auto scores = query("SELECT "
+                              "0,notes,score,fast,slow,maxcombo,addtime,exscore,lamp,pgreat,great,good,bad,bpoor,miss,"
+                              "bp,cb,playedtime,replay "
+                              "FROM score_cache_bms WHERE md5 = ?",
+                              {hash.hexdigest()});
+    if (scores.empty())
+        return nullptr;
+    assert(scores.size() == 1);
+    assert(convertCachedPbBms(*out, scores[0]));
+    return out;
+}
+
+void ScoreDB::saveChartScoreBmsToHistory(const HashMD5& hash, const ScoreBMS& score)
+{
+    const auto lamp = static_cast<int>(score.lamp);
+    const auto play_time = score.play_time.norm();
+    int ret = exec("INSERT INTO score_history_bms"
+                   "(md5,notes,score,fast,slow,maxcombo,addtime,exscore,lamp,pgreat,great,good,bad,bpoor,miss,bp,"
+                   "cb,playedtime,replay) "
+                   "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                   {hash.hexdigest(), score.notes, score.score, score.fast, score.slow, score.maxcombo, score.addtime,
+                    score.exscore, lamp, score.pgreat, score.great, score.good, score.bad, score.kpoor, score.miss,
+                    score.bp, score.combobreak, play_time, score.replayFileName});
+    assert(ret == SQLITE_OK);
+}
+
+void ScoreDB::updateCachedChartPbBms(const HashMD5& hash, const ScoreBMS& score)
+{
+    const std::string hashStr = hash.hexdigest();
+
+    if (const std::shared_ptr<ScoreBMS> pRecord = fetchCachedPbBMS(hash); pRecord)
+    {
+        auto record = *pRecord;
+
+        if (score.exscore > record.exscore)
+        {
+            record.rate = score.rate;
+            record.fast = score.fast;
+            record.slow = score.slow;
+            record.exscore = score.exscore;
+            record.pgreat = score.pgreat;
+            record.great = score.great;
+            record.good = score.good;
+            record.bad = score.bad;
+            record.kpoor = score.kpoor;
+            record.miss = score.miss;
+            record.combobreak = score.combobreak;
+            record.replayFileName = score.replayFileName;
+        }
+        else if (score.exscore == record.exscore)
+        {
+            if (score.maxcombo > record.maxcombo || score.bp < record.bp || (int)score.lamp > (int)record.lamp)
+                record.replayFileName = score.replayFileName;
+        }
+
+        record.bp = std::min(record.bp, score.bp);
+        record.clearcount += score.clearcount;
+        record.maxcombo = std::max(record.maxcombo, score.maxcombo);
+        record.notes = score.notes;
+        record.playcount += score.playcount;
+        record.score = std::max(record.score, score.score);
+
+        if ((int)score.lamp > (int)record.lamp)
+        {
+            record.lamp = score.lamp;
+        }
+
+        record.play_time += score.play_time;
+
+        const auto play_time = record.play_time.norm();
+        exec("UPDATE score_cache_bms SET "
+             "notes=?,score=?,fast=?,slow=?,maxcombo=?,addtime=?,pc=?,clearcount=?,exscore=?,lamp=?,pgreat=?,"
+             "great=?,good=?,bad=?,bpoor=?,miss=?,bp=?,cb=?,playedtime=?,replay=? WHERE md5=?",
+             {record.notes,   record.score,     record.fast,       record.slow,    record.maxcombo,
+              record.addtime, record.playcount, record.clearcount, record.exscore, (int)record.lamp,
+              record.pgreat,  record.great,     record.good,       record.bad,     record.kpoor,
+              record.miss,    record.bp,        record.combobreak, play_time,      record.replayFileName,
+              hashStr});
+        cache["score_bms"].insert_or_assign(hashStr, std::make_shared<ScoreBMS>(std::move(record)));
+    }
+    else
+    {
+        const auto play_time = score.play_time.norm();
+        exec("INSERT INTO score_cache_bms"
+             "(md5,notes,score,fast,slow,maxcombo,addtime,pc,clearcount,exscore,lamp,pgreat,great,good,bad,bpoor,"
+             "miss,bp,cb,playedtime,replay) "
+             "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+             {hashStr,          score.notes,     score.score,         score.fast,    score.slow,      score.maxcombo,
+              score.addtime,    score.playcount, score.clearcount,    score.exscore, (int)score.lamp, score.pgreat,
+              score.great,      score.good,      score.bad,           score.kpoor,   score.miss,      score.bp,
+              score.combobreak, play_time,       score.replayFileName});
+        cache["score_bms"].insert_or_assign(hashStr, std::make_shared<ScoreBMS>(score));
+    }
+}
+
+static bool convertHistoryScoreBms(ScoreBMS& out, const std::vector<std::any>& score) noexcept
+try
+{
+    if (score.size() < 19) return false;
+    // md5 = ANY_STR(score[0]);
+    out.notes = ANY_INT(score[1]);
+    out.score = ANY_INT(score[2]);
+    out.fast = ANY_INT(score[3]);
+    out.slow = ANY_INT(score[4]);
+    out.maxcombo = ANY_INT(score[5]);
+    out.addtime = ANY_INT(score[6]);
+    out.exscore = ANY_INT(score[7]);
+    out.lamp = static_cast<ScoreBMS::Lamp>(ANY_INT(score[8]));
+    out.pgreat = ANY_INT(score[9]);
+    out.great = ANY_INT(score[10]);
+    out.good = ANY_INT(score[11]);
+    out.bad = ANY_INT(score[12]);
+    out.kpoor = ANY_INT(score[13]);
+    out.miss = ANY_INT(score[14]);
+    out.bp = ANY_INT(score[15]);
+    out.combobreak = ANY_INT(score[16]);
+    out.play_time = ANY_INT(score[17]);
+    out.replayFileName = ANY_STR(score[18]);
+
+    out.rate = static_cast<double>(out.exscore) / static_cast<double>(out.notes * 2) * 100;
+    return true;
+}
+catch (const std::exception& e)
+{
+    LOG_ERROR << e.what();
+    return false;
+}
+
+void ScoreDB::rebuildBmsPbCache()
+{
+    LOG_DEBUG << "[ScoreDB] Asked to rebuild BMS score PB cache";
+    transactionStart();
+    exec("DELETE FROM score_cache_bms");
+    for (const auto& raw_score : query("SELECT * FROM score_bms ORDER BY addtime"))
+    {
+        auto score = std::make_shared<ScoreBMS>();
+        const std::string md5 = ANY_STR(raw_score[0]);
+        assert(convert_score_bms(*score, raw_score));
+        updateCachedChartPbBms(md5, *score);
+    }
+    for (const auto& raw_score : query("SELECT "
+                                       "md5,notes,score,fast,slow,maxcombo,addtime,exscore,lamp,pgreat,great,good,bad,"
+                                       "bpoor,miss,bp,cb,playedtime,replay "
+                                       "FROM score_history_bms "
+                                       "ORDER BY addtime"))
+    {
+        auto score = std::make_shared<ScoreBMS>();
+        const std::string md5 = ANY_STR(raw_score[0]);
+        assert(convertHistoryScoreBms(*score, raw_score));
+        updateCachedChartPbBms(md5, *score);
+    }
+    transactionStop();
+    preloadScore();
 }
 
 void ScoreDB::deleteCourseScoreBMS(const HashMD5& hash)
 {
-    deleteScoreBMS("score_course_bms", hash);
+    deleteLegacyScoreBMS("score_course_bms", hash);
 }
 
 std::shared_ptr<ScoreBMS> ScoreDB::getCourseScoreBMS(const HashMD5& hash) const
@@ -314,23 +589,26 @@ std::shared_ptr<ScoreBMS> ScoreDB::getCourseScoreBMS(const HashMD5& hash) const
 
 void ScoreDB::updateCourseScoreBMS(const HashMD5& hash, const ScoreBMS& score)
 {
-    updateScoreBMS("score_course_bms", hash, score);
+    updateLegacyScoreBMS("score_course_bms", hash, score);
 }
 
 void ScoreDB::preloadScore()
 {
     cache.clear();
-    for (auto& r : query("SELECT * FROM score_course_bms"))
+    for (const auto& r : query("SELECT * FROM score_course_bms"))
     {
-        auto ret = std::make_shared<ScoreBMS>();
-        convert_score_bms(*ret, r);
-        cache["score_course_bms"].insert_or_assign(ANY_STR(r[0]), std::move(ret));
+        auto score = std::make_shared<ScoreBMS>();
+        assert(convert_score_bms(*score, r));
+        cache["score_course_bms"].insert_or_assign(ANY_STR(r[0]), std::move(score));
     }
-    for (auto& r : query("SELECT * FROM score_bms"))
+    for (const auto& r : query("SELECT "
+                               "md5,notes,score,fast,slow,maxcombo,addtime,exscore,lamp,pgreat,great,good,bad,bpoor,"
+                               "miss,bp,cb,playedtime,replay "
+                               "FROM score_cache_bms"))
     {
-        auto ret = std::make_shared<ScoreBMS>();
-        convert_score_bms(*ret, r);
-        cache["score_bms"].insert_or_assign(ANY_STR(r[0]), std::move(ret));
+        auto score = std::make_shared<ScoreBMS>();
+        assert(convertCachedPbBms(*score, r));
+        cache["score_bms"].insert_or_assign(ANY_STR(r[0]), std::move(score));
     }
 }
 

src/db/db_score.h
@@ -51,23 +51,32 @@ public:
     ScoreDB& operator= (ScoreDB&) = delete;
 
 protected:
-    void deleteScoreBMS(const char* tableName, const HashMD5& hash);
+    void deleteLegacyScoreBMS(const char* tableName, const HashMD5& hash);
+    void updateLegacyScoreBMS(const char* tableName, const HashMD5& hash, const ScoreBMS& score);
+
     [[nodiscard]] std::shared_ptr<ScoreBMS> getScoreBMS(const char* tableName, const HashMD5& hash) const;
-    void updateScoreBMS(const char* tableName, const HashMD5& hash, const ScoreBMS& score);
+
+    [[nodiscard]] std::shared_ptr<ScoreBMS> fetchCachedPbBMS(const HashMD5& hash) const;
+    void saveChartScoreBmsToHistory(const HashMD5& hash, const ScoreBMS& score);
+    void updateCachedChartPbBms(const HashMD5& hash, const ScoreBMS& score);
 
 public:
     void deleteChartScoreBMS(const HashMD5& hash);
     [[nodiscard]] std::shared_ptr<ScoreBMS> getChartScoreBMS(const HashMD5& hash) const;
-    void updateChartScoreBMS(const HashMD5& hash, const ScoreBMS& score);
+    void insertChartScoreBMS(const HashMD5& hash, const ScoreBMS& score);
 
     void deleteCourseScoreBMS(const HashMD5& hash);
     [[nodiscard]] std::shared_ptr<ScoreBMS> getCourseScoreBMS(const HashMD5& hash) const;
     void updateCourseScoreBMS(const HashMD5& hash, const ScoreBMS& score);
 
+    void rebuildBmsPbCache();
     void preloadScore();
 
     [[nodiscard]] lunaticvibes::OverallStats getStats();
 
+    // Test things, don't normally use:
+
+    void updateLegacyChartScoreBMS(const HashMD5& hash, const ScoreBMS& score);
 private:
     void updateStats(const ScoreBMS& score);
 };

src/game/scene/scene_result.cpp
@@ -354,7 +354,7 @@ void SceneResult::updateFadeout()
                     score->combobreak = rBMS->getJudgeCountEx(RulesetBMS::JUDGE_CB);
                 }
 
-                g_pScoreDB->updateChartScoreBMS(gChartContext.hash, *score);
+                g_pScoreDB->insertChartScoreBMS(gChartContext.hash, *score);
                 pScore = score;
 
                 break;

src/game/scene/scene_select_imgui.cpp
@@ -547,6 +547,20 @@ void SceneSelect::imguiPageOptionsGeneral()
             imgui_add_profile_popup = true;
         }
 
+        // TODO: translations.
+        if (ImGui::Button("Rebuild score cache"))
+        {
+            g_pScoreDB->rebuildBmsPbCache();
+            for (size_t idx = 0; idx < gSelectContext.entries.size(); ++idx)
+            {
+                std::unique_lock<std::shared_mutex> u(gSelectContext._mutex);
+                updateEntryScore(idx);
+                setEntryInfo();
+            }
+        }
+
+        // TODO: rebuild profile stats button.
+
         ImGui::Spacing();
         ImGui::Separator();
 

test/db/test_score_db.cpp
@@ -20,7 +20,8 @@ TEST(ScoreDb, ChartScoreUpdating)
         score.exscore = 1;
         score.lamp = ScoreBMS::Lamp::NOPLAY;
         score.pgreat = 2;
-        score_db.updateChartScoreBMS(hash, score);
+        score.replayFileName = "score1";
+        score_db.insertChartScoreBMS(hash, score);
         EXPECT_EQ(score_db.getChartScoreBMS(hash)->exscore, 1);
         EXPECT_EQ(score_db.getStats().pgreat, 0);
         EXPECT_EQ(score_db.getStats().play_count, 0);
@@ -32,7 +33,8 @@ TEST(ScoreDb, ChartScoreUpdating)
         score.exscore = 2;
         score.lamp = ScoreBMS::Lamp::FAILED;
         score.pgreat = 2;
-        score_db.updateChartScoreBMS(hash, score);
+        score.replayFileName = "score2";
+        score_db.insertChartScoreBMS(hash, score);
         EXPECT_EQ(score_db.getChartScoreBMS(hash)->exscore, 2);
         EXPECT_EQ(score_db.getStats().pgreat, 2);
         EXPECT_EQ(score_db.getStats().play_count, 1);
@@ -44,7 +46,8 @@ TEST(ScoreDb, ChartScoreUpdating)
         score.exscore = 1;
         score.lamp = ScoreBMS::Lamp::EASY;
         score.pgreat = 2;
-        score_db.updateChartScoreBMS(hash, score);
+        score.replayFileName = "score3";
+        score_db.insertChartScoreBMS(hash, score);
         EXPECT_EQ(score_db.getChartScoreBMS(hash)->exscore, 2);
         EXPECT_EQ(score_db.getStats().pgreat, 4);
         EXPECT_EQ(score_db.getStats().play_count, 2);
@@ -100,7 +103,9 @@ TEST(ScoreDb, ChartScoreDeleting)
 
     EXPECT_EQ(score_db.getChartScoreBMS(hash), nullptr);
 
-    score_db.updateChartScoreBMS(hash, score);
+    // And also test for old DB table. Same scores themselves are never supposed to be in both.
+    score_db.updateLegacyChartScoreBMS(hash, score);
+    score_db.insertChartScoreBMS(hash, score);
     ASSERT_NE(score_db.getChartScoreBMS(hash), nullptr);
     EXPECT_EQ(score_db.getChartScoreBMS(hash)->exscore, 1);
 
@@ -108,7 +113,7 @@ TEST(ScoreDb, ChartScoreDeleting)
     EXPECT_EQ(score_db.getChartScoreBMS(hash), nullptr);
 
     // Not just cache that was invalidated.
-    score_db.preloadScore();
+    score_db.rebuildBmsPbCache();
     EXPECT_EQ(score_db.getChartScoreBMS(hash), nullptr);
 }
 
@@ -130,6 +135,6 @@ TEST(ScoreDb, CourseScoreDeleting)
     EXPECT_EQ(score_db.getCourseScoreBMS(hash), nullptr);
 
     // Not just cache that was invalidated.
-    score_db.preloadScore();
+    score_db.rebuildBmsPbCache();
     EXPECT_EQ(score_db.getChartScoreBMS(hash), nullptr);
 }

test/game/test_scene_select.cpp
@@ -33,7 +33,7 @@ TEST(SceneSelect, SearchQueryScoreDeletionWorks)
     ScoreBMS chart_score;
     chart_score.exscore = 1;
     EXPECT_EQ(score_db.getChartScoreBMS(chart->fileHash), nullptr);
-    score_db.updateChartScoreBMS(chart->fileHash, chart_score);
+    score_db.insertChartScoreBMS(chart->fileHash, chart_score);
     EXPECT_EQ(score_db.getChartScoreBMS(chart->fileHash)->exscore, 1);
 
     ScoreBMS course_score;

commit 88a1e54a47119d7f4650e90d9f4242ab1b368033

fix(db): unwrap type converting calls from asserts
Fixes Release builds, as assert calls are removed outside of Debug
builds.

Fixes: 4ff61ab2e3de ("feat: store all and every chart score")
Fixes: c55125ba4a2e ("refactor(db): check for database errors")

src/db/db_score.cpp
@@ -350,7 +350,7 @@ void ScoreDB::updateLegacyScoreBMS(const char* tableName, const HashMD5& hash, c
     assert(!result.empty());
     const auto& r = result[0];
     auto scoreRefetched = std::make_shared<ScoreBMS>();
-    assert(convert_score_bms(*scoreRefetched, r));
+    convert_score_bms(*scoreRefetched, r);
     cache[tableName].insert_or_assign(hashStr, std::move(scoreRefetched));
 }
 
@@ -430,7 +430,7 @@ catch (const std::exception& e)
     if (scores.empty())
         return nullptr;
     assert(scores.size() == 1);
-    assert(convertCachedPbBms(*out, scores[0]));
+    convertCachedPbBms(*out, scores[0]);
     return out;
 }
 
@@ -559,7 +559,7 @@ void ScoreDB::rebuildBmsPbCache()
     {
         auto score = std::make_shared<ScoreBMS>();
         const std::string md5 = ANY_STR(raw_score[0]);
-        assert(convert_score_bms(*score, raw_score));
+        convert_score_bms(*score, raw_score);
         updateCachedChartPbBms(md5, *score);
     }
     for (const auto& raw_score : query("SELECT "
@@ -570,7 +570,7 @@ void ScoreDB::rebuildBmsPbCache()
     {
         auto score = std::make_shared<ScoreBMS>();
         const std::string md5 = ANY_STR(raw_score[0]);
-        assert(convertHistoryScoreBms(*score, raw_score));
+        convertHistoryScoreBms(*score, raw_score);
         updateCachedChartPbBms(md5, *score);
     }
     transactionStop();
@@ -598,7 +598,7 @@ void ScoreDB::preloadScore()
     for (const auto& r : query("SELECT * FROM score_course_bms"))
     {
         auto score = std::make_shared<ScoreBMS>();
-        assert(convert_score_bms(*score, r));
+        convert_score_bms(*score, r);
         cache["score_course_bms"].insert_or_assign(ANY_STR(r[0]), std::move(score));
     }
     for (const auto& r : query("SELECT "
@@ -607,7 +607,7 @@ void ScoreDB::preloadScore()
                                "FROM score_cache_bms"))
     {
         auto score = std::make_shared<ScoreBMS>();
-        assert(convertCachedPbBms(*score, r));
+        convertCachedPbBms(*score, r);
         cache["score_bms"].insert_or_assign(ANY_STR(r[0]), std::move(score));
     }
 }