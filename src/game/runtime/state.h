chown2/lunaticvibesf

commit fe506e3a0f1c4ab28b6b4239f5f5a1b3c06984f9

fix(game): add mutex for state getting/setting
Should fix AddressSanitizer: heap-use-after-free
State::get[abi:cxx11](IndexText)
SpriteText::updateText()
SkinBase::update()

I've only had this once ever so can't say for sure.

src/game/runtime/state.h
@@ -10,6 +10,8 @@
 #include "index/timer.h"
 
 #include <array>
+#include <mutex>
+#include <shared_mutex>
 #include <string>
 #include <string_view>
 
@@ -37,6 +39,7 @@ protected:
 	private:
 		std::array<Value, _size> _data;
 		std::array<Value, _size> _dataDefault;
+		std::shared_mutex _mutex;
 
 	public:
 		Value get(Key n) const
@@ -44,6 +47,7 @@ protected:
 			size_t idx = (size_t)n;
 			if (idx < _size)
 			{
+				std::shared_lock l{_mutex};
 				return _data.data()[idx];
 			}
 			return Value();
@@ -54,6 +58,7 @@ protected:
 			size_t idx = (size_t)n;
 			if (idx < _size)
 			{
+				std::unique_lock l{_mutex};
 				_data[idx] = value;
 				return true;
 			}

commit 48c95a4ec7031c9a2ad77c2ca63fbaf33d445785

fix(game): make mutex member variable 'mutable' to allow const methods
Fixes compilation error :
> passing ‘std::shared_lock<const std::shared_mutex>::mutex_type’ {aka
> ‘const std::shared_mutex’} as ‘this’ argument discards qualifiers

Fixes: fe506e3a0f1c ("fix(game): add mutex for state getting/setting")

src/game/runtime/state.h
@@ -39,7 +39,7 @@ protected:
 	private:
 		std::array<Value, _size> _data;
 		std::array<Value, _size> _dataDefault;
-		std::shared_mutex _mutex;
+		mutable std::shared_mutex _mutex;
 
 	public:
 		Value get(Key n) const